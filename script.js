2f6b3aa7ed26b5e40cf0c9bee8d8f5fd98628daf
amirreza <45253403+amirkhoshnejad@users.noreply.github.com>
2025-11-16 23:34:44 +0330

    final edit

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let addList=[];
-let todonum=0;
-const btn=document.getElementById('addTaskBtn');
-const text=document.getElementById('iputarray');
-const output=document.getElementById('output');
-btn.addEventListener('click',()=>{
-    let taskList = {};
-    taskList.input=text.value;
-    addList.push(taskList);
-    output.innerHTML += `<div class="hi">${taskList.input}</div>`;
-    text.value='';
-    document.getElementById('chill').style.display='none';
+let addList = [];
+let todonum = 0;
+const btn = document.getElementById("addTaskBtn");
+const text = document.getElementById("iputarray");
+const output = document.getElementById("output");
+btn.addEventListener("click", () => {
+  let taskList = {};
+  taskList.input = text.value;
+  addList.push(taskList);
+  todonum++;
+  output.innerHTML += `
+    <div class="hi">
+      <span class="num">${todonum}:</span>
+      <span class="text">${taskList.input}</span>
+    </div>
+  `;
+  /* output.innerHTML += `<div class="hi"> ${taskList.input}</div>`;
+  outnum.innerHTML += `<div class="hi">${todonum}:`;*/
+  text.value = "";
+
+  document.getElementById("chill").style.display = "none";
 });
